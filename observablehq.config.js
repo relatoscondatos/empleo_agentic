9e6144dc9807809f8913a2ed845d92ac9ef48966
Ernesto Laval <ernesto.laval@gmail.com>
2025-04-04 13:55:05 -0300

    Format

--- observablehq.config.js
+++ observablehq.config.js
@@ -17,22 +17,21 @@ export default {
   // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
-  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
+  head: '<link rel="icon" href="favicon.ico" type="image/png" sizes="32x32">',
 
   // The path to the source root.
   root: "src",
 
   // Some additional configuration options and their defaults:
-  // theme: "default", // try "light", "dark", "slate", etc.
-  // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
-  // sidebar: true, // whether to show the sidebar
-  // toc: true, // whether to show the table of contents
-  // pager: true, // whether to show previous & next links in the footer
+  theme: "default", // try "light", "dark", "slate", etc.
+  header: 'Un relato con datos <img src="./favicon.ico" height="20px"> <a href="https://www.relatoscondatos.cl" target="_blank">www.relatoscondatos.cl</a> ', // what to show in the header (HTML)
+  footer: 'Relatos con datos <i class="fas fa-envelope"></i> <a href="mailto:contacto@relatoscondatos.cl">contacto@relatoscondatos.cl</a>', // what to show in the footer (HTML)
+  sidebar: false, // whether to show the sidebar
+  toc: {show:true, label:"Contenido"}, // whether to show the table of contents
+  pager: false, // whether to show previous & next links in the footer
   // output: "dist", // path to the output root for build
   // search: true, // activate search
   // linkify: true, // convert URLs in Markdown to links
-  // typographer: false, // smart quotes and other typographic improvements
-  // preserveExtension: false, // drop .html from URLs
-  // preserveIndex: false, // drop /index from URLs
+  typographer: true, // smart quotes and other typographic improvements
+  // cleanUrls: true, // drop .html from URLs
 };
